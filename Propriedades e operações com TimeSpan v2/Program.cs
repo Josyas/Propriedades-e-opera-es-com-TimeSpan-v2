025ea3471ccdc63a99b6be6084fbae0b9537fcb1
Josias <87546094+Josyas@users.noreply.github.com>
2022-01-20 22:42:23 -0300

    projeto

---
+++ Propriedades e operações com TimeSpan v2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Propriedades_e_operações_com_TimeSpan_v2 {
+    class Program {
+        static void Main(string[] args) {
+
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sun = t1.Add(t2);
+            TimeSpan dif = t1.Subtract(t2);
+            TimeSpan mult = t2.Multiply(2.0);
+            TimeSpan div = t2.Divide(2.0);
+
+            Console.WriteLine(sun);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
+        }
+    }
+}
